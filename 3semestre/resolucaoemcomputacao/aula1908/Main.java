dad810877ab2d5e69d3708f18f6cc51f048b0107
Vinicius Trevisan <vinimtrevisan@gmail.com>
2025-08-26 18:51:31 -0300

    oi

--- 3semestre/resolucaoemcomputacao/aula1908/Main.java
+++ 3semestre/resolucaoemcomputacao/aula1908/Main.java
@@ -8,6 +8,6 @@ public class Main {
         SuperHeroi p1 = new SuperHeroi("Vinicius", "M", 1.71,
                 Arrays.asList("Voar", "Super forca", "Super velocidade"));
         p1.usarHabilidades();
-        System.out.println(p1);}
-
+        System.out.println(p1);
+    }
 }
